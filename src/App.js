96d02e8d0c9803974859f503726ffa928dc03414
VeselaDzhuvtova <108488627+VeselaDzhuvtova@users.noreply.github.com>
2023-03-24 17:08:03 +0200

    Login 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
 import * as carService from './services/carService';
+import { AuthContext } from './contexts/authContext';
 
 import { Fragment } from 'react';
 import Header from './components/Header/Header';
@@ -13,55 +14,61 @@ import Insurances from './components/Insurances/Insurances';
 import Companies from './components/Companies/Companies';
 import Calculator from './components/Calculator/Calculator';
 import Offers from './components/Offers/Offers';
-import Login from './components/Login/Login';
+import { Login } from './components/Login/Login';
 import Register from './components/Register/Register';
 import Footer from './components/Footer/Footer';
-import CatalogItem  from './components/Catalog/CatalogItem/CatalogItem';
+import CatalogItem from './components/Catalog/CatalogItem/CatalogItem';
 import Details from './components/Details/Details';
 
 function App() {
     const navigate = useNavigate();
     const [cars, setCars] = useState([]);
+    const [auth, setAuth] = useState({});
 
     useEffect(() => {
         carService.getAll()
-        .then(result => {
-            setCars(result);
-        })
+            .then(result => {
+                setCars(result);
+            })
     }, []);
 
     const onCreateCarSubmit = async (data) => {
-        
+
         const newCar = await carService.create(data);
-    
+
         //TODO add to state
         setCars(state => [...state, newCar]);
-        
+
         //TODO redirect to catalog
         navigate('/catalog')
-    }
+    };
+
+    const onLoginSubmit = async (data) => {
+        console.log(data)
+    };
 
     return (
-        <Fragment>
-            <Header />
-            <main>
-                <Routes>
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/' element={<Home />} />
-                    <Route path='/catalog' element={<Catalog cars={cars} />} />
-                    <Route path='/catalog/CatalogItem' element={<CatalogItem />} />
-                    <Route path='/create' element={<Create onCreateCarSubmit={onCreateCarSubmit} />} />
-                    <Route path='/insurances' element={<Insurances />} />
-                    <Route path='/companies' element={<Companies />} />
-                    <Route path='/calculator' element={<Calculator />} />
-                    <Route path='/offers' element={<Offers />} />
-                    <Route path='/catalog/:carId' element={<Details />} />
-                </Routes>
-            </main>
-            <Footer />
-        </Fragment>
-
+        <AuthContext.Provider value={{onLoginSubmit}}>
+            <Fragment>
+                <Header />
+                <main>
+                    <Routes>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/' element={<Home />} />
+                        <Route path='/catalog' element={<Catalog cars={cars} />} />
+                        <Route path='/catalog/CatalogItem' element={<CatalogItem />} />
+                        <Route path='/create' element={<Create onCreateCarSubmit={onCreateCarSubmit} />} />
+                        <Route path='/insurances' element={<Insurances />} />
+                        <Route path='/companies' element={<Companies />} />
+                        <Route path='/calculator' element={<Calculator />} />
+                        <Route path='/offers' element={<Offers />} />
+                        <Route path='/catalog/:carId' element={<Details />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </Fragment>
+        </AuthContext.Provider>
     );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,46 @@
-const Login = () => {
+import { useContext } from "react";
+
+import { AuthContext } from "../../contexts/authContext";
+import { useForm } from "../../hooks/useForm";
+
+
+const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password'
+};
+
+export const Login = () => {
+    const { onLoginSubmit } = useContext(AuthContext);
+    const { values, changeHandler } = useForm({
+        [LoginFormKeys.Email]: '',
+        [LoginFormKeys.Password]: '',
+    }, onLoginSubmit);
+
     return (
         <div className="login-box">
-  <h1>Вход</h1>
-  <form method="POST">
-    <label>Имейл</label>
-    <input type="text" name="email" placeholder="Email.." />
-    <label>Парола</label>
-    <input type="password" name="password" placeholder="Password.." />
-    <input type="submit" defaultValue="Submit" />
-  </form>
-  <p>
-    Нямаш акаунт? <a href="/Register">Регистрация</a>
-  </p>
-</div>
+            <h1>Вход</h1>
+            <form id="login" method="POST" onSubmit={onLoginSubmit}>
+                <label>Имейл</label>
+                <input type="text"
+                    name={LoginFormKeys.Email}
+                    placeholder="Email.."
+                    value={values[LoginFormKeys.Email]}
+                    onChange={changeHandler}
+                />
+                <label>Парола</label>
+                <input type="password"
+                    name={LoginFormKeys.Password}
+                    placeholder="Password.."
+                    value={values[LoginFormKeys.Password]}
+                    onChange={changeHandler}
+                />
+                <input type="submit" className="btn_submit" value="Login" />
+            </form>
+            <p>
+                Нямаш акаунт? <a href="/Register">Регистрация</a>
+            </p>
+        </div>
 
     );
 };
 
-export default Login;
